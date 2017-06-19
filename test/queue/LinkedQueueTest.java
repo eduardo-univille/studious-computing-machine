db864b1c75aeb14642303730cae6b74717c9ad27
Eduardo Augusto Klosowski <eduardo.klosowski@univille.br>
2017-06-19 20:22:03 -0300

    LinkedQueue

---
+++ test/queue/LinkedQueueTest.java
@@ -0,0 +1,47 @@
+package queue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LinkedQueueTest {
+    @Test
+    public void enqueueTest() {
+        Queue<Integer> queue = new LinkedQueue<>();
+        Assert.assertTrue(queue.isEmpty());
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        Assert.assertEquals(3, queue.getSize());
+        Assert.assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void dequeueTest() {
+        Queue<Integer> queue = new LinkedQueue<>();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        Assert.assertEquals(Integer.valueOf(1), queue.dequeue());
+        Assert.assertEquals(Integer.valueOf(2), queue.dequeue());
+        Assert.assertEquals(Integer.valueOf(3), queue.dequeue());
+    }
+
+    @Test
+    public void frontTest() {
+        Queue<Integer> queue = new LinkedQueue<>();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        Assert.assertEquals(Integer.valueOf(1), queue.front());
+        Assert.assertEquals(3, queue.getSize());
+
+        Assert.assertEquals(Integer.valueOf(1), queue.front());
+        Assert.assertEquals(3, queue.getSize());
+    }
+}
